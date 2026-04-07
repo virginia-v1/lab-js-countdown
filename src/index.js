efbeacba078f04e35075f7b083301d8b0cc7ef81
Virginia Vanstavel <virginiavanstavel@Virginias-MacBook-Pro.local>
2026-04-07 18:13:08 +0200

    final lab

--- src/index.js
+++ src/index.js
@@ -5,28 +5,46 @@ let timer = null; // Variable to store the interval
 
 
 // ITERATION 1: Add event listener to the start button
+const startButton = document.querySelector('#start-btn');
+function startCountdown() {
+  console.log("startCountdown called!");
+}
+startButton.addEventListener ("click", () => {
+  startCountdown(); //don't forget the brackets or it won't work
+})
 
-// Your code goes here ...
-
-
-
+const htmlTimeElement = document.querySelector('#time');
 
 // ITERATION 2: Start Countdown
+
 function startCountdown() {
   console.log("startCountdown called!");
 
+  let timerDuration = 10;
 
-  // Your code goes here ...
+  const intervalId = setInterval(() => {
+    htmlTimeElement.innerHTML = timerDuration;
+
+    if (timerDuration === 0) {
+      clearInterval(intervalId);
+      console.log("showToast");
+      return;
+    }
+    timerDuration--;
+
+  }, 1000);
 }
-
-
 
 
 // ITERATION 3: Show Toast
 function showToast(message) {
   console.log("showToast called!");
+  const toastElement = document.querySelector ('#toast');
+  toastElement.classList.add("show");
 
-  // Your code goes here ...
+  
+
+  
 
 
 
